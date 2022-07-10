36fda24f68d195e4f98de980343b9e2dc0c9b8d0
Gerson Bravo Herrera <brayangbh478@gmail.com>
2022-07-10 17:19:39 -0500

    Proyecto Integrador

---
+++ src/main/java/pe/org/chaclacayo/pyfinal2/controller/HomeController.java
@@ -0,0 +1,29 @@
+
+package pe.org.chaclacayo.pyfinal2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author admin
+ */
+@Controller
+public class HomeController {
+    
+    @GetMapping
+    public String index(){
+        return "login";
+    }
+    @GetMapping("/register")
+    public String register(){
+        return "formRegister";       
+     
+    }
+    
+    @GetMapping("/contrasena")
+    public String contrasena(){
+        return "contrasena";       
+     
+    }
+}
---
+++ src/main/java/pe/org/chaclacayo/pyfinal2/controller/MenuController.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.org.chaclacayo.pyfinal2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author admin
+ */
+@Controller
+@RequestMapping("/auth/main")
+public class MenuController {
+    
+    @GetMapping
+    public String main(){
+        return "main";
+    }
+    
+    @GetMapping("/mainmp")
+    public String mainmp(){
+        return "mainmp";
+    }
+    
+     @GetMapping("/mainpv")
+    public String mainpv(){
+        return "mainpv";
+    }
+    
+     @GetMapping("/mainds")
+    public String mainds(){
+        return "mainds";
+    }
+    
+        @GetMapping("/registrar")
+    public String registrar(){
+        return "registrar";
+    }
+        @GetMapping("/registrar2")
+    public String registrar2(){
+        return "registrar2";
+    }
+    
+    @GetMapping("/perfil")
+    public String perfil(){
+        return "perfil";
+    }
+}
